815c36253326e3a48b83d96ad9e9b58aea9f69a6
xingzhi <lvyaojia@gmail.com>
2012-08-12 18:06:47 +0800

    已能正常收缩注释

--- js/main.js
+++ js/main.js
@@ -1,54 +1,156 @@
  jQuery(document).ready(function($) {
-            $('.sentence').after('<span class="fullmark">。</span>');
 
             $('.sentence').click(
                 function(){
                     showComment($(this));
                 }
             );
+
         });
 
 function showComment(currentSentence){
+
+    commentID = currentSentence.attr('comment');
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: 'http://sutra.sinaapp.com/'+commentID+'/',
+        success: function(data){
+            alert(data[0].fields)
+        }
+    }) 
+
     currentSentence.each(function(){
-        
+        var self = $(this);
+
+
+        //点击的对象右边的经文向右移动，同时隐藏起来
         $(this).parent().prevAll().animate(
             {
-                left:$(this).parent().index()*76+120,
-            },1000
+               left:$(this).parent().index()*76+120-37,
+            },1000,function(){
+                self.parent().prevAll().hide();
+            }
         );
 
+        //点击的对象左边的经文向左移动
         $(this).parent().nextAll().animate(
             {
                 right:500,
             },1000
         );
+
+        //点击的对象向右移动，同时改变padding
         $(this).parent().animate(
             {
-                paddingLeft: 0,
-                /*marginRight: 39,*/
-                zIndex:99,
-                left:$(this).parent().index()*76+120,
+                left:$(this).parent().index()*76+120-37,
             },1000,function(){
-                $('.commentWrapper').fadeIn(200);
+                $('#'+commentID).fadeIn(200);
             }
         );
+
+        //点击对象改变颜色为红色
         $(this).animate(
             {
                 color:'#c30d23'
             },800
         );
+
+        //改变同列其他经文颜色为淡灰色
         $(this).next().addClass('currentFullMark');
         $(this).siblings(':not(.currentFullMark)').animate(
             {
                 color:'#B5B5B6'
             },800
         );
-        $('.sentence').unbind('click');
+        $(this).siblings().children().animate(
+            {
+                color:'#B5B5B6'
+            },800
+        );
+
+        //$('.sentence').unbind('click');
+        self.unbind('click');
+        self.siblings().unbind('click');
+        self.click(function(){
+            showSutra(self);
+        });
         
-        /*setTimeout(function(){
-            $(':not(.commentWrapper)').click(function(){
-                alert(1111);
-            });
-        }, 1000);*/
     });
+
+}
+
+
+function showSutra(currentSentence) {
+
+    commentID = currentSentence.attr('comment');
+
+    $('#'+commentID).fadeOut(300,function(){
+        currentSentence.each(function(){
+            var self = $(this);
+
+            $(this).parent().prevAll().css("display","inline-block");
+
+            $(this).parent().prevAll().animate(
+                {
+                   left:0
+                },1000
+            );
+
+            $(this).parent().nextAll().animate(
+                {
+                    right:0,
+                },1000
+            );
+            $(this).parent().nextAll().removeAttr('style');
+
+            $(this).parent().animate(
+                {   
+                    left:0,
+                },1000,function(){
+                    currentSentence.parent().children().removeAttr('style');
+                    currentSentence.parent().parent().children().removeAttr('style');
+                }
+            );
+            $(this).parent().removeAttr('style');
+
+            $(this).animate(
+                {
+                    color:'#000'
+                },800
+            );
+
+            $(this).next().removeClass('currentFullMark');
+            $(this).siblings(':not(.fullmark)').animate(
+                {
+                    color:'#000'
+                },800
+            );
+            $(this).siblings('.fullmark').animate(
+                {
+                    color:'#c30d23'
+                },800
+            );
+
+            $(this).siblings().children().animate(
+                {
+                    color:'#c30d23'
+                },800
+            );
+
+
+            self.parent().children().unbind('click');
+            self.parent().children().each(function(){
+                $(this).click(function(){
+                    showComment($(this));
+                });
+                
+            });
+
+        });
+    });
+
+    
+
 }
